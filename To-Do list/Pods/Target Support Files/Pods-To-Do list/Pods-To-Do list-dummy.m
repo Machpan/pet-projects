3c737d4e06508313972190bb2861410aa9d40278
Machpan <machetik@gmail.com>
2022-07-03 18:10:16 +0300

    create new project without Storyboards, added Snaplit framework

---
+++ To-Do list/Pods/Target Support Files/Pods-To-Do list/Pods-To-Do list-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_To_Do_list : NSObject
+@end
+@implementation PodsDummy_Pods_To_Do_list
+@end
---
+++ To-Do list/To-Do list/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do list
+//
+//  Created by Владимир Осипов on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
