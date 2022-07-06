24525cc5e2478930586b334de1d448550e4bf0d1
Machpan <machetik@gmail.com>
2022-07-06 20:12:12 +0300

    deleted extra folder

--- 01 - EmojiNotes/01 - EmojiNotes/Models/Constants.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Constants{
-    static let cellID = "emojiCell"
-    static let editCellSegueID = "editEmoji"
-    static let saveButtonSegueID = "saveSegue"
-    private init(){}
-}
--- 01 - EmojiNotes/01 - EmojiNotes/Models/Objects.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Objects{
-    var emoji: String
-    var title: String
-    var description: String
-    var isFavourite: Bool
-}
